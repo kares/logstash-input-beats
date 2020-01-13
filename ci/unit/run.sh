b609279d1e6a6b035a8a00a7b754292c8824c0fb
Karol Bucek <self@kares.org>
2020-01-13 17:00:38 +0100

    CI: shared unit (wout integration) tests

--- ci/unit/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# This is intended to be run inside the docker container as the command of the docker-compose.
-set -ex
-
-bundle exec rspec spec
-bundle exec rake test:integration:setup
-bundle exec rspec spec --tag integration
